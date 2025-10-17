4342b7a7114ed1691e3c389db6933a6c2b18fc72
eloizecosta <eloizrcosta@gmail.com>
2025-10-17 15:38:13 -0300

    Add localized file names and enhance menuPrincipal styling

--- src/app/(tabs)/menuPrincipal.tsx
+++ src/app/(tabs)/menuPrincipal.tsx
@@ -1,9 +1,29 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function EsqueciSenha(){
     return(
-        <View>
-            <Text>Menu principal</Text>
+        <View style={styles.container}>
+            <Text style={styles.title}>Menu principal</Text>
         </View>
     )
 }
+
+
+const styles = StyleSheet.create({
+        container: {
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+            gap: 32,
+    },
+    title: {
+        fontSize: 12,
+        marginTop: ,
+        color: "#fff",
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+   
+   
+   
+})
