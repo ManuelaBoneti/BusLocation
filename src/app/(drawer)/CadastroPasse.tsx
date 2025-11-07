6a77c07eacccdbd9b64b32e36b94373a16b55393
RafaelaVitoria0 <rafaelavitoriafabricio@gmail.com>
2025-11-07 15:11:10 -0300

    refactor: update Avaliacao component with star rating and submission logic

--- src/app/(drawer)/CadastroPasse.tsx
+++ src/app/(drawer)/CadastroPasse.tsx
@@ -85,33 +85,3 @@ const styles = StyleSheet.create({
 })
 
 
-import { Back } from "@/components/Back";
-import { StyleSheet, Text, View } from "react-native";
-
-export default function CadastroPasse(){
-    return(
-        <View style={styles.container}>
-            <Back />
-            <View style={styles.content}>
-                <Text style={styles.title}>Página</Text>
-            </View>
-        </View>
-    )
-}
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        backgroundColor: "#033b85"
-    },
-    content: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        width: '100%',
-    },
-    title: {
-        color: "#fff",
-        fontSize: 30,
-    },
-})
