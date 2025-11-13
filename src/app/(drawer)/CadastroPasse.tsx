05dec66ead5a9a0aa06d580952ec1b0c4325bba8
eloizecosta <eloizrcosta@gmail.com>
2025-11-13 10:58:05 -0300

    Refactor CadastroPasse component: remove unused imports, streamline layout, and enhance code readability.

--- src/app/(drawer)/CadastroPasse.tsx
+++ src/app/(drawer)/CadastroPasse.tsx
@@ -1,4 +1,5 @@
 
+import { BackNavigation } from "@/components/BackNavigation";
 import { Button } from "@/components/Button";
 import { MaterialIcons } from "@expo/vector-icons";
 import { useState } from "react";
@@ -10,9 +11,6 @@ import {
   View,
   Modal,
 } from "react-native";
-import { Image, StyleSheet, Text, TextInput, TouchableOpacity, View} from "react-native";
-
-
 
 export default function CadastroPasse() {
   const [nota, setNota] = useState(0);
@@ -34,16 +32,6 @@ export default function CadastroPasse() {
     <View style={styles.container}>
       <BackNavigation />
       <Text style={styles.title}>Cadastro do Passe</Text>
-export default function CadastroPasse(){
-
-    return(
-        <View style={styles.container}>
-            <Image 
-                            style={styles.tela} source={require('@/assets/telaInicial.png')} 
-                        />
-         
-            <Text style={styles.title}>Cadastro do Passe</Text>
-
 
       <View style={styles.section}>
         <View style={styles.inputContainer}>
@@ -60,11 +48,6 @@ export default function CadastroPasse(){
           <MaterialIcons style={styles.icon} name="search" size={16} />
           <TextInput style={styles.textInput} placeholder="Código do passe físico:" placeholderTextColor="#aaa" />
         </View>
-                <View style={styles.inputContainer}>
-                    <MaterialIcons style={styles.icon} name='search' size={16} />
-                    <TextInput style={styles.textInput} placeholder="Código do passe fisíco:" />
-                </View>
-                   <Button />
 
         <TouchableOpacity style={styles.button} onPress={PasseCadastrado}>
           <Text style={styles.tituloButton}>Cadastrar</Text>
@@ -189,60 +172,3 @@ const styles = StyleSheet.create({
     fontWeight: "600",
   },
 });
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        backgroundColor: "#033b85",
-    },
-    icon: {
-        margin: 10
-    },
-    title: {
-        fontSize: 35,
-        color: "#fff",
-        marginTop: 120,
-        marginBottom: -250,
-        paddingBottom: 50,
-        fontFamily: 'Quicksand_700Bold', 
-        textAlign: 'center',
-  },
-
-    tela : {
-        width: 340,
-        height: 370,
-        marginBottom: -240,
-    },
-    section: {
-        backgroundColor: '#ffff',
-        borderRadius: 10,
-        padding: 15,
-        elevation: 2,
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 1 },
-        shadowOpacity: 0.1,
-        shadowRadius: 2,
-        width: 300,
-        height: 400,
-        marginBlock: 250,
-    },
-    inputContainer: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        borderColor: '#ccc',
-        borderWidth: 1,
-        borderRadius: 8,
-        paddingHorizontal: 10,
-        marginBottom: 20,
-        marginTop: 20,
-        backgroundColor: '#fff'
-    },
-    textInput: {
-        flex: 1,
-        height: 60,
-        marginLeft: 5,
-        fontSize: 18,
-        color: "white",
-        fontWeight: "bold"
-    }
-})
-
--- src/app/(drawer)/passeVirtual.tsx
+++ src/app/(drawer)/passeVirtual.tsx
@@ -92,7 +92,7 @@ import { useRef, useState } from "react";
           ))}
         </View>
       </View>
-    </View>
+  
   );
 }
  
