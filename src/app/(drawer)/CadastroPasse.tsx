1493a94e429b214a48593e1701058e7718592e54
eloizecosta <eloizrcosta@gmail.com>
2025-11-13 14:48:32 -0300

    Add alert modal and validation to CadastroPasse component for user feedback

--- src/app/(drawer)/CadastroPasse.tsx
+++ src/app/(drawer)/CadastroPasse.tsx
@@ -2,11 +2,28 @@
 import { BackNavigation } from "@/components/BackNavigation";
 import { Button } from "@/components/Button";
 import { MaterialIcons } from "@expo/vector-icons";
-import { Image, StyleSheet, Text, TextInput, TouchableOpacity, View} from "react-native";
+import { useState } from "react";
+import { Image, Modal, StyleSheet, Text, TextInput, TouchableOpacity, View} from "react-native";
 
 
 
 export default function CadastroPasse(){
+
+  const [nota, setNota] = useState(0);
+  const [alertVisible, setAlertVisible] = useState(false);
+  const [alertMessage, setAlertMessage] = useState("");
+  
+
+  const PasseCadastrado = () => {
+    if (nota === 0) {
+      setAlertMessage("Preencha todos os campos acima para criar o cadastro.");
+      setAlertVisible(true);
+    } else {
+      setAlertMessage("Cadastro concluído!");
+      setAlertVisible(true);
+      setNota(0);
+    }
+  };
 
     return(
         <View style={styles.container}>
@@ -112,6 +129,53 @@ const styles = StyleSheet.create({
         fontSize: 18,
         color: "white",
         fontWeight: "bold"
-    }
+    },
+    // alert
+  overlay: {
+    flex: 1,
+    backgroundColor: "rgba(0,0,0,0.5)",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  alertBox: {
+    width: 300,
+    backgroundColor: "rgb(255, 255, 255)",
+    borderRadius: 16,
+    padding: 20,
+    alignItems: "center",
+    elevation: 5,
+    shadowColor: "#000",
+    shadowOpacity: 0.2,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 4,
+  },
+  alertText: {
+    fontSize: 16,
+    textAlign: "center",
+    color: "#333",
+    marginBottom: 20,
+  },
+  okButton: {
+    paddingVertical: 8,
+    paddingHorizontal: 30,
+  },
+  okText: {
+    fontSize: 16,
+    color: "rgb(3, 83, 223)",
+    fontWeight: "600",
+  },
+  tituloButton:{
+    fontSize: 20,
+    color: "#ffffff",
+    fontWeight: 'bold',
+    textAlign: 'center',
+},
+button:{
+    backgroundColor: "#677db0",
+    borderRadius: 12,
+    paddingVertical: 10,
+    paddingHorizontal: 30,
+    marginTop: 10,
+},
 })
 
