bee8647c117b22a6c7af72cf611c2b6a60af67e5
eloizecosta <eloizrcosta@gmail.com>
2025-11-13 14:38:59 -0300

    Enhance Avaliacoes component: add modal for alerts, improve user feedback on evaluation submission, and streamline alert handling.

--- src/app/(drawer)/avaliacoes.tsx
+++ src/app/(drawer)/avaliacoes.tsx
@@ -1,5 +1,4 @@
-import {View,Text, StyleSheet, Alert, Image} from "react-native";
-
+import {View,Text, StyleSheet, Alert, Image, Modal} from "react-native";
 import { TouchableOpacity } from "react-native";
 import { BackNavigation } from "@/components/BackNavigation";
 import { useState } from "react";
@@ -7,21 +6,22 @@ import StarRating from "@/components/Back/Star";
 
 
 export default function avaliacoes(){
-    const [nota, setNota] = useState(0);
 
-    const AvaliacaoEnviada = () =>{
-        if(nota === 0){
-            Alert.alert(
-            "Selecione uma nota antes de enviar a avaliação.");
-        
-        }else
-        Alert.alert(
-            "Avaliação enviada com sucesso!"
-            );
-            setNota(0);
-        
+  const [nota, setNota] = useState(0);
+  const [alertVisible, setAlertVisible] = useState(false);
+  const [alertMessage, setAlertMessage] = useState("");
+  
 
-    };
+  const AvaliacaoEnviada = () => {
+    if (nota === 0) {
+      setAlertMessage("Preencha a avaliação antes de enviar.");
+      setAlertVisible(true);
+    } else {
+      setAlertMessage("Avaliação enviada com sucesso!");
+      setAlertVisible(true);
+      setNota(0);
+    }
+  };
     return(
         <View style={styles.container}>
             <BackNavigation/>
@@ -38,9 +38,26 @@ export default function avaliacoes(){
                     Avaliar agora
                 </Text>
             </TouchableOpacity>
+        {/* alert */}
+              <Modal transparent visible={alertVisible} animationType="fade">
+                <View style={styles.overlay}>
+                  <View style={styles.alertBox}>
+                    <Text style={styles.alertText}>{alertMessage}</Text>
+        
+                    <TouchableOpacity
+                      style={styles.okButton}
+                      onPress={() => setAlertVisible(false)}
+                    >
+                      <Text style={styles.okText}>OK</Text>
+                    </TouchableOpacity>
+                  </View>
+                </View>
+              </Modal>
+            
 
 
         </View>
+        
     )
 }
 
@@ -81,5 +98,39 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         textAlign: 'center',
     },
+    // alert
+  overlay: {
+    flex: 1,
+    backgroundColor: "rgba(0,0,0,0.5)",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  alertBox: {
+    width: 300,
+    backgroundColor: "rgb(255, 255, 255)",
+    borderRadius: 16,
+    padding: 20,
+    alignItems: "center",
+    elevation: 5,
+    shadowColor: "#000",
+    shadowOpacity: 0.2,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 4,
+  },
+  alertText: {
+    fontSize: 16,
+    textAlign: "center",
+    color: "#333",
+    marginBottom: 20,
+  },
+  okButton: {
+    paddingVertical: 8,
+    paddingHorizontal: 30,
+  },
+  okText: {
+    fontSize: 16,
+    color: "rgb(3, 83, 223)",
+    fontWeight: "600",
+  },
 
 })
